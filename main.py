6b6c0f6ce020d1ad55984ffe7170c76e939acae1
PrabhashDiss <prabhashdissanayake2k@gmail.com>
2024-07-25 17:23:42 +0000

    Add model training, and interface setup

--- main.py
+++ main.py
@@ -22,3 +22,32 @@ if not os.path.exists(db_file):
     # Commit changes and close the connection
     conn.commit()
     conn.close()
+
+from vanna.remote import VannaDefault
+vn = VannaDefault(model='autoquery', api_key=os.getenv('VANNA_API_KEY'))
+
+vn.connect_to_sqlite(db_file)
+
+df_ddl = vn.run_sql("SELECT type, sql FROM sqlite_master WHERE sql is not null")
+
+for ddl in df_ddl['sql'].to_list():
+  vn.train(ddl=ddl)
+
+vn.train(documentation="This database is designed to handle the everyday operations of a major car company. It provides functionality to view the transactions of dealers, track car inventory, and maintain the models and avaliable options of cars.")
+
+# Read the SQL file
+with open('Queries.sql', 'r') as file:
+    sql_script = file.read()
+
+# Split the script by semicolons to get individual queries
+queries = sql_script.split(';')
+
+# Execute each query and process results
+for query in queries:
+    query = query.strip()
+    if query:
+        vn.train(sql=query)
+
+from vanna.flask import VannaFlaskApp
+app = VannaFlaskApp(vn, allow_llm_to_see_data=True)
+app.run()
