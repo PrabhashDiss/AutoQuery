1e60caac40ad98dff265f26557a4680eacfd20e7
PrabhashDiss <prabhashdissanayake2k@gmail.com>
2024-07-25 17:20:07 +0000

    Update SQL script filenames

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ conn = sqlite3.connect('my_database.db')
 cursor = conn.cursor()
 
 # Execute the SQL commands from create.sql
-with open('create.sql', 'r') as file:
+with open('Create_Tables.sql', 'r') as file:
     create_sql = file.read()
 cursor.executescript(create_sql)
 
 # Execute the SQL commands from insert.sql
-with open('insert.sql', 'r') as file:
+with open('Insert.sql', 'r') as file:
     insert_sql = file.read()
 cursor.executescript(insert_sql)
 
